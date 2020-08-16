4cf9d747edb92e9e0fe191cdea093c9e14e48fc1
sid <sidcley.siqueira@gmail.com>
2020-08-16 15:37:04 -0300

    Exercício - Implementação na primeira aplicação django. Retornando um Hello World

--- core/views.py
+++ core/views.py
@@ -4,3 +4,22 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos<h1>'.format(nome, idade))
+
+def soma(request, valor1, valor2):
+    resultado = valor1 + valor2
+    return HttpResponse('<h1>A soma de {} - {} = {}<h1>'.format(valor1, valor2, resultado))
+
+def subtracao(request, valor1, valor2):
+    resultado = valor1 - valor2
+    return HttpResponse('<h1>A subtrção de {} - {} = {}<h1>'.format(valor1, valor2, resultado))
+
+def multiplicacao(request, valor1, valor2):
+    resultado = valor1 * valor2
+    return HttpResponse('<h1>A multiplicação de {} * {} = {}<h1>'.format(valor1, valor2, resultado))
+
+def divisao(request, valor1, valor2):
+    resultado = valor1 / valor2
+    if valor1 % valor2 == 0:
+        return HttpResponse('<h1>A divisão de {} / {} = {}<h1>'.format(valor1, valor2, int(resultado)))
+    else:
+        return HttpResponse('<h1>A divisão de {} / {} = {}<h1>'.format(valor1, valor2, round(resultado, 2)))
